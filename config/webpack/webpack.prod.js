d811808ace07bb312a2ce20cb5d86accbbb79632
Gregory Spraggins <gregoryspragginsjr@gmail.com>
2022-10-19 17:21:27 -0400

    Fix to home hero image. Deprecating webpack production

--- config/webpack/webpack.prod.js
+++ config/webpack/webpack.prod.js
@@ -4,16 +4,16 @@ const TerserPlugin = require('terser-webpack-plugin');
 const baseConfig = require('./webpack.base.js');
 
 const prodConfig = {
-  mode: 'production',
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin(),
-    ],
-  },
-  plugins: [
-    new OptimizeCssAssetsPlugin(),
-  ]
+  // mode: 'production',
+  // optimization: {
+  //   minimize: true,
+  //   minimizer: [
+  //     new TerserPlugin(),
+  //   ],
+  // },
+  // plugins: [
+  //   new OptimizeCssAssetsPlugin(),
+  // ]
 };
 
 module.exports = merge(
