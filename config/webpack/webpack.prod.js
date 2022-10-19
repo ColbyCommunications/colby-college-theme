f50067cc78332c72589a9bbd1d9f8781d0cb9082
Brandon K. Waltz <bwaltz6@gmail.com>
2022-10-19 15:05:32 -0400

    fix prod build

--- config/webpack/webpack.prod.js
+++ config/webpack/webpack.prod.js
@@ -4,19 +4,15 @@ const TerserPlugin = require('terser-webpack-plugin');
 const baseConfig = require('./webpack.base.js');
 
 const prodConfig = {
-  mode: 'production',
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin(),
-    ],
-  },
-  plugins: [
-    new OptimizeCssAssetsPlugin(),
-  ]
+    mode: 'production',
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    },
+    plugins: [new OptimizeCssAssetsPlugin()],
 };
 
 module.exports = merge(
-  baseConfig,
-  prodConfig
+    baseConfig
+    // prodConfig
 );
