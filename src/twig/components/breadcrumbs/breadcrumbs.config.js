f4c80927635d59b44e8cde338887e6d202648691
Gregory Spraggins <gregoryspragginsjr@gmail.com>
2022-11-03 14:15:26 -0400

    Addressing overflow issue for long lists in breadcrumbs on mobile

--- src/twig/components/breadcrumbs/breadcrumbs.config.js
+++ src/twig/components/breadcrumbs/breadcrumbs.config.js
@@ -15,6 +15,18 @@ module.exports = {
         title: 'Trustees',
         url: 'https://welcometruth.com/',
       },
+      {
+        title: 'Lorem Ipsum',
+        url: 'https://welcometruth.com/',
+      },
+      {
+        title: 'Dolar Set Amit',
+        url: 'https://welcometruth.com/',
+      },
+      {
+        title: 'Test Link',
+        url: 'https://welcometruth.com/',
+      },
     ],
   }
 }
