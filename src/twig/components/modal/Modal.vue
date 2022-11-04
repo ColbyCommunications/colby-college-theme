988764584d7d39cb8a1ca0d317cda15a63d8aceb
Brandon K. Waltz <bwaltz6@gmail.com>
2022-11-04 09:52:52 -0400

    overflow-auto on modal

--- src/twig/components/modal/Modal.vue
+++ src/twig/components/modal/Modal.vue
@@ -1,45 +1,51 @@
 <template>
-  <vue-final-modal
-    v-model="showModal"
-    :class="classes"
-    classes="modal__container flex justify-center items-center px-5"
-    content-class="modal__content relative w-full max-w-xl max-h-96 overflow-hidden bg-white rounded overflow-scroll"
-  >
-    <button class="absolute top-5 right-3" @click="showModal = false">
-      <span class="relative block w-6 h-0.5 bg-indigo transition-all duration-200 ease-in-out bg-transparent">
-        <span class="absolute left-0 w-full h-full bg-indigo origin-center top-[-8px] transition-all duration-200 ease-in-out !top-0 rotate-45" />
-        <span class="absolute left-0 w-full h-full bg-indigo origin-center top-[8px] transition-all duration-200 ease-in-out !top-0 rotate-[-45deg]" />
-      </span>
+    <vue-final-modal
+        v-model="showModal"
+        :class="classes"
+        classes="modal__container flex justify-center items-center px-5"
+        content-class="modal__content relative w-full max-w-xl max-h-96 overflow-hidden bg-white rounded overflow-auto"
+    >
+        <button class="absolute top-5 right-3" @click="showModal = false">
+            <span
+                class="relative block w-6 h-0.5 bg-indigo transition-all duration-200 ease-in-out bg-transparent"
+            >
+                <span
+                    class="absolute left-0 w-full h-full bg-indigo origin-center top-[-8px] transition-all duration-200 ease-in-out !top-0 rotate-45"
+                />
+                <span
+                    class="absolute left-0 w-full h-full bg-indigo origin-center top-[8px] transition-all duration-200 ease-in-out !top-0 rotate-[-45deg]"
+                />
+            </span>
+        </button>
+        <slot name="content" />
+    </vue-final-modal>
+    <button class="text-left" @click="showModal = true">
+        <slot name="button" />
     </button>
-    <slot name="content" />
-  </vue-final-modal>
-  <button class="text-left" @click="showModal = true">
-    <slot name="button" />
-  </button>
 </template>
 
 <script>
-import { $vfm, VueFinalModal, ModalsContainer } from 'vue-final-modal';
+    import { $vfm, VueFinalModal, ModalsContainer } from 'vue-final-modal';
 
-export default {
-  components: {
-    VueFinalModal,
-    ModalsContainer
-  },
-  data() {
-    return {
-      showModal: false,
+    export default {
+        components: {
+            VueFinalModal,
+            ModalsContainer,
+        },
+        data() {
+            return {
+                showModal: false,
+            };
+        },
+        props: {
+            full: {
+                type: Boolean,
+                required: false,
+            },
+            classes: {
+                type: String,
+                required: false,
+            },
+        },
     };
-  },
-  props: {
-    full: {
-      type: Boolean,
-      required: false,
-    },
-    classes: {
-      type: String,
-      required: false,
-    },
-  }
-}
 </script>
