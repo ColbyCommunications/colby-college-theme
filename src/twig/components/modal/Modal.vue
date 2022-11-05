55205f5d846a649370f688374bda2cb8966d0751
Gregory Spraggins <gregoryspragginsjr@gmail.com>
2022-11-05 17:10:03 -0400

    Search experience ported to a modal experience

--- src/twig/components/modal/Modal.vue
+++ src/twig/components/modal/Modal.vue
@@ -2,10 +2,14 @@
   <vue-final-modal
     v-model="showModal"
     :class="classes"
-    classes="modal__container flex justify-center items-center px-5"
-    content-class="modal__content relative w-full max-w-xl max-h-96 overflow-hidden bg-white rounded overflow-scroll"
+    :classes="{'!px-0': this.full == true, 'modal__container flex justify-center items-center px-5': true}"
+    :content-class="{'w-full max-w-none h-full max-h-none': this.full == true, 'max-w-xl max-h-96 rounded': !this.full, 'modal__content relative w-full overflow-hidden bg-white overflow-scroll': true }"
   >
-    <button class="absolute top-5 right-3" @click="showModal = false">
+    <button
+      class="absolute top-5 right-3"
+      :class="{'right-5': this.full == true}"
+      @click="showModal = false"
+    >
       <span class="relative block w-6 h-0.5 bg-indigo transition-all duration-200 ease-in-out bg-transparent">
         <span class="absolute left-0 w-full h-full bg-indigo origin-center top-[-8px] transition-all duration-200 ease-in-out !top-0 rotate-45" />
         <span class="absolute left-0 w-full h-full bg-indigo origin-center top-[8px] transition-all duration-200 ease-in-out !top-0 rotate-[-45deg]" />
@@ -13,8 +17,15 @@
     </button>
     <slot name="content" />
   </vue-final-modal>
-  <button class="text-left" @click="showModal = true">
-    <slot name="button" />
+  <button
+    class="text-left group active"
+    :class="{'[&>span]:text-indigo-1000 [&>span>svg]:fill-indigo-1000': this.showModal == true}"
+    @click="showModal = !showModal"
+  >
+    <slot
+      name="button"
+      :showModal="showModal"
+    />
   </button>
 </template>
 
