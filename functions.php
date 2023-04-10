c2af9042b27c11f685a918c819bf852f00f09f2c
Brandon K. Waltz <bwaltz6@gmail.com>
2023-04-10 10:39:31 -0400

    debug

--- functions.php
+++ functions.php
@@ -1134,9 +1134,9 @@ function getNewPeople( $directory_data ) {
 	}
 }
 
-add_action( 'gform_after_submission_12', 'update_directory_profile', 10, 2 );
+add_action( 'gform_after_submission_13', 'update_directory_profile', 10, 2 );
 function update_directory_profile( $entry, $form ) {
-
+	die( var_dump( $entry ) );
 	// get attributes from SimpleSAML session
 	$as         = new \SimpleSAML\Auth\Simple( 'default-sp' );
 	$attributes = $as->getAttributes();
