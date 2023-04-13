bb2eef0a667336f238ffc189a2b963e8f52fcfcb
Brandon K. Waltz <bwaltz6@gmail.com>
2023-04-13 13:32:17 -0400

    remove

--- simplesaml-local/metadata/saml20-idp-hosted.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$metadata['https://colby.lndo.site/simplesaml/saml2/idp/metadata.php/default-sp'] = array(
-	'host' => '__DEFAULT__',
-	'auth' => 'default-sp',
-);
--- simplesaml-local/metadata/saml20-idp-remote.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$metadata['http://www.okta.com/exkqw4m28ezS0DYyx357'] = array(
-	'SingleSignOnService' => 'https://okta.colby.edu/app/colby_colbydirectorysettingswebpage_1/exkqw4m28ezS0DYyx357/sso/saml',
-);
