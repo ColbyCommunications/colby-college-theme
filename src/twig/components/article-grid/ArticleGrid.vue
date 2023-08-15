4968eb74737b751d9456d1ed02414dbbc38832e8
Giovanni Aceto <55949483+GioAceto@users.noreply.github.com>
2023-08-15 16:57:11 -0400

    modify article grid

--- src/twig/components/article-grid/ArticleGrid.vue
+++ src/twig/components/article-grid/ArticleGrid.vue
@@ -1,134 +1,179 @@
 <template>
-  <div>
-    <slot v-if="!renderApi" />
-    <div
-      v-if="renderApi && api == 'president'"
-      class="article-grid grid grid-cols-8 gap-10 max-w-screen-2xl w-full my-0 mx-auto"
-      :class="{
-        'grid-cols-9': columns == 3,
-      }"
-    >
-      <div
-        v-for="(item, index) in data"
-        class="article-grid__item glide__slide col-span-4"
-        :class="{
-          'md:col-span-2 col-span-4': columns == 4,
-          'md:col-span-3 col-span-9': columns == 3,
-        }"
-        :key="index"
-      >
-        <article
-          class="article space-y-4"
-          :class="{ 'pt-1 border-t-2 border-solid border-indigo-600': border }"
+    <div>
+        <slot v-if="!renderApi" />
+        <div
+            v-if="renderApi"
+            class="article-grid grid gap-10 max-w-screen-2xl w-full my-0 mx-auto"
+            :class="{
+                'grid-cols-9': columns == 3,
+                'grid-cols-8': columns == 4 || columns == 2,
+            }"
         >
-          <div class="context w-full py-4">
-            <component is="text-group" class="text-group">
-              <div
-                v-if="item.date"
-                class="text-group__subheading font-extended font-bold text-12 leading-130 tracking-8 text-left text-azure uppercase"
-                v-text="item.date"
-              />
-              <h2
-                class="text-group__heading font-extended font-normal text-20 leading-110 -tracking-3 text-left text-indigo"
+            <div
+                v-for="(item, index) in data"
+                class="article-grid__item glide__slide col-span-4"
                 :class="{
-                  'mt-2': item.date,
+                    'md:col-span-2 col-span-4': columns == 4,
+                    'md:col-span-3 col-span-9': columns == 3,
                 }"
-                v-text="decodeHtmlEntities(item.title.rendered)"
-              />
-              <p
-                class="text-group__p font-body font-normal text-14 leading-130 text-left text-indigo-800 mt-2"
-                v-text="item['post-meta-fields'].summary[0]"
-              />
-            </component>
-            <div class="button-group flex flex-wrap gap-4 mt-4">
-              <a
-                class="btn group inline-flex flex-row items-center space-x-1.5 rounded border border-solid border-indigo-300 font-body font-normal text-10 leading-130 text-indigo bg-indigo-100 hover:bg-indigo-200 focus:bg-indigo-200 focus:outline focus:outline-2 focus:outline-canary outline-offset-[-1px] py-1 px-3 transition-all duration-200 ease-in-out !no-underline"
-                :href="item.url"
-              >
-                <span class="btn__text">
-                  {{ cta }}
-                  <div
-                    class="btn__border block bg-indigo h-px w-0 group-hover:w-full transition-all duration-200 ease-in-out"
-                  ></div>
-                </span>
-              </a>
+                :key="index"
+            >
+                <article
+                    class="article space-y-4"
+                    :class="{ 'pt-1 border-t-2 border-solid border-indigo-600': border }"
+                >
+                    <div class="context w-full py-4">
+                        <component is="text-group" class="text-group flex">
+                            <div class="w-full flex flex-col justify-start">
+                                <img width="96" height="96" :src="item.image" :alt="item.title" />
+                            </div>
+                            <div>
+                                <h2
+                                    class="text-group__heading font-extended font-normal text-20 leading-110 -tracking-3 text-left text-indigo"
+                                    v-text="decodeHtmlEntities(item.title.rendered)"
+                                />
+                                <p
+                                    class="text-group__p font-body font-normal text-14 leading-130 text-left text-indigo-800 mt-2"
+                                    v-text="item['post-meta-fields'].summary[0]"
+                                />
+                            </div>
+                        </component>
+                        <div class="button-group flex flex-wrap gap-4 mt-4">
+                            <a
+                                class="btn group inline-flex flex-row items-center space-x-1.5 rounded border border-solid border-indigo-300 font-body font-normal text-10 leading-130 text-indigo bg-indigo-100 hover:bg-indigo-200 focus:bg-indigo-200 focus:outline focus:outline-2 focus:outline-canary outline-offset-[-1px] py-1 px-3 transition-all duration-200 ease-in-out !no-underline"
+                                :href="item.url"
+                            >
+                                <span class="btn__text">
+                                    {{ cta }}
+                                    <div
+                                        class="btn__border block bg-indigo h-px w-0 group-hover:w-full transition-all duration-200 ease-in-out"
+                                    ></div>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </article>
             </div>
-          </div>
-        </article>
-      </div>
+        </div>
     </div>
-  </div>
 </template>
 <script>
-import axios from "axios";
-import moment from "moment";
-import TextGroup from "/src/twig/components/text-group/TextGroup.vue";
+    import axios from 'axios';
+    import moment from 'moment';
+    import TextGroup from '/src/twig/components/text-group/TextGroup.vue';
 
-export default {
-  components: {
-    TextGroup,
-  },
-  data() {
-    return {
-      data: [],
+    export default {
+        components: {
+            TextGroup,
+        },
+        data() {
+            return {
+                data: [],
+            };
+        },
+        async mounted() {
+            if (this.renderApi) {
+                this.endpoint =
+                    'https://dev-54ta5gq-4nvswumupeimi.us-4.platformsh.site/wp-json/custom/v1/external-posts';
+
+                await axios.get(this.endpoint).then((output) => {
+                    this.data = output.data
+                        .filter((item) => {
+                            switch (this.posts) {
+                                case 'stories':
+                                    //
+                                    break;
+                                case 'media_coverage':
+                                    if (this.api === 'all_media_hp') {
+                                        return (
+                                            item.story_type &&
+                                            Array.isArray(item.story_type) &&
+                                            item.story_type[0].slug === 'media-coverage' &&
+                                            item.content.rendered
+                                        );
+                                    } else if (this.api === 'all_media') {
+                                        return (
+                                            item.story_type &&
+                                            Array.isArray(item.story_type) &&
+                                            item.story_type[0].slug === 'media-coverage' &&
+                                            item.content.rendered
+                                        );
+                                    } else if (this.api === 'president') {
+                                        return (
+                                            item.story_type &&
+                                            Array.isArray(item.story_type) &&
+                                            item.story_type[0].slug === 'media-coverage' &&
+                                            item.content.rendered &&
+                                            item.tags &&
+                                            item.tags.some((tag) => tag.name === 'president')
+                                        );
+                                    }
+                                    break;
+                                case 'faculty_accomplishments':
+                                    ///
+                                    break;
+                                case 'videos':
+                                    ///
+                                    break;
+                            }
+                            return false;
+                        })
+                        .map((item) => {
+                            return {
+                                title: {
+                                    rendered: item.title.rendered.replace(/<\/?[^>]+(>|$)/g, ''),
+                                },
+                                'post-meta-fields': {
+                                    summary: [
+                                        `${item.content.rendered
+                                            .replace(/<\/?[^>]+(>|$)/g, '')
+                                            .substring(0, 120)}...`,
+                                    ],
+                                },
+                                url: item.external_url,
+                                image: item.image,
+                                date: moment(item.date).format('MMM DD, YYYY'),
+                            };
+                        })
+                        .slice(0, this.range);
+                });
+            }
+        },
+        props: {
+            renderApi: {
+                required: true,
+            },
+            posts: {
+                type: String,
+                required: false,
+            },
+            api: {
+                type: String,
+                required: false,
+            },
+            range: {
+                type: Number,
+                required: false,
+            },
+            border: {
+                type: String,
+                required: false,
+            },
+            cta: {
+                type: String,
+                required: false,
+                default: 'Read Story',
+            },
+            columns: {
+                type: Number,
+                required: false,
+            },
+        },
+        methods: {
+            decodeHtmlEntities(input) {
+                const doc = new DOMParser().parseFromString(input, 'text/html');
+                return doc.documentElement.textContent;
+            },
+        },
     };
-  },
-  async mounted() {
-    if (this.renderApi) {
-      switch (this.api) {
-        case "president":
-          this.endpoint =
-            "https://news.colby.edu/wp-json/wp/v2/external_post?&tags=577&_embed=1";
-          break;
-      }
-
-      await axios.get(this.endpoint).then((output) => {
-        this.data = output.data.map((item) => {
-          return {
-            title: {
-              rendered: item.title.rendered.replace(/<\/?[^>]+(>|$)/g, ""),
-            },
-            "post-meta-fields": {
-              summary: [
-                `${item.content.rendered
-                  .replace(/<\/?[^>]+(>|$)/g, "")
-                  .substring(0, 120)}...`,
-              ],
-            },
-            url: item.external_url,
-            date: moment(item.date).format("MMM DD, YYYY"),
-          };
-        });
-      });
-    }
-  },
-  props: {
-    renderApi: {
-      required: true,
-    },
-    api: {
-      type: String,
-      required: false,
-    },
-    border: {
-      type: String,
-      required: false,
-    },
-    cta: {
-      type: String,
-      required: false,
-      default: "Read Story",
-    },
-    columns: {
-      type: Number,
-      required: false,
-    },
-  },
-  methods: {
-    decodeHtmlEntities(input) {
-      const doc = new DOMParser().parseFromString(input, "text/html");
-      return doc.documentElement.textContent;
-    },
-  },
-};
 </script>
