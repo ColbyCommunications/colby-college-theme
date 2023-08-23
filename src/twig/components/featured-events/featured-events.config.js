cd90b2deb4d0e6529b12e949c404a8aa1b1251c4
Gregory Spraggins <gregoryspragginsjr@gmail.com>
2023-08-23 19:51:39 -0400

    Change request for carousel in featured events component for Arts

--- src/twig/components/featured-events/featured-events.config.js
+++ src/twig/components/featured-events/featured-events.config.js
@@ -2,30 +2,84 @@ module.exports = {
   title: 'Featured Events',
   status: 'wip',
   context: {
-    subheading: 'Featured Events',
-    heading: 'Clever Tagline',
+    heading: 'Featured Events',
     paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam congue pulvinar lectus.',
-    buttons: [
+    items: [
       {
-        title: 'All events',
-        url: 'https://welcometruth.com/',
-      }
-    ],
-    featured: {
-      image: {
-        srcset: 'https://via.placeholder.com/800x800',
-        src: 'https://via.placeholder.com/400x400',
-        alt: 'Test alt',
+        subheading: 'Kicker 1',
+        heading: 'Lorem ipsum dolor sit amet, consectet.',
+        paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam congue pulvinar lectus.',
+        buttons: [
+          {
+            url: 'https://welcometruth.com/',
+            title: 'Read Story',
+          },
+        ],
+        image: {
+          srcset: 'https://via.placeholder.com/760x430',
+          src: 'https://via.placeholder.com/410x290',
+          alt: 'Test alt',
+        },
       },
-      subheading: 'June, 18, 2023',
-      heading: 'Exhibition Tour: Ashley Bryan/Paula Wilson: Take the World into Your Arms',
-      paragraph: 'Join us for a free tour today, offered at 11 am and 1 pm, in the Joan Dignam Schmaltz Gallery of Art at the Paul J. Schupf Art Center in downtown Waterville.',
-      buttons: [
-        {
-          title: 'See Event',
-          url: 'https://welcometruth.com/',
-        }
-      ],
-    }
+      {
+        subheading: 'Kicker 2',
+        heading: 'Lorem ipsum dolor sit amet, consectet.',
+        paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam congue pulvinar lectus.',
+        buttons: [
+          {
+            url: 'https://welcometruth.com/',
+            title: 'Read Story',
+          },
+        ],
+        image: {
+          srcset: 'https://via.placeholder.com/760x430',
+          src: 'https://via.placeholder.com/410x290',
+          alt: 'Test alt',
+        },
+      },
+      {
+        subheading: 'Kicker 3',
+        heading: 'Lorem ipsum dolor sit amet, consectet.',
+        paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam congue pulvinar lectus.',
+        buttons: [
+          {
+            url: 'https://welcometruth.com/',
+            title: 'Read Story',
+          },
+        ],
+        image: {
+          srcset: 'https://via.placeholder.com/760x430',
+          src: 'https://via.placeholder.com/410x290',
+          alt: 'Test alt',
+        },
+      },
+    ]
   }
+  // context: {
+  //   subheading: 'Featured Events',
+  //   heading: 'Clever Tagline',
+  //   paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam congue pulvinar lectus.',
+  //   buttons: [
+  //     {
+  //       title: 'All events',
+  //       url: 'https://welcometruth.com/',
+  //     }
+  //   ],
+  //   featured: {
+  //     image: {
+  //       srcset: 'https://via.placeholder.com/800x800',
+  //       src: 'https://via.placeholder.com/400x400',
+  //       alt: 'Test alt',
+  //     },
+  //     subheading: 'June, 18, 2023',
+  //     heading: 'Exhibition Tour: Ashley Bryan/Paula Wilson: Take the World into Your Arms',
+  //     paragraph: 'Join us for a free tour today, offered at 11 am and 1 pm, in the Joan Dignam Schmaltz Gallery of Art at the Paul J. Schupf Art Center in downtown Waterville.',
+  //     buttons: [
+  //       {
+  //         title: 'See Event',
+  //         url: 'https://welcometruth.com/',
+  //       }
+  //     ],
+  //   }
+  // }
 }
