549b2ce4fd6453e68d91064690e89d6e698c5165
Brandon K. Waltz <bwaltz6@gmail.com>
2023-09-25 12:47:02 -0400

    adds components.js file

---
+++ src/components.js
@@ -0,0 +1,45 @@
+import Header from './twig/components/header/Header.vue';
+import OverlayHero from './twig/components/overlay-hero/OverlayHero.vue';
+import Carousel from './twig/components/carousel/Carousel.vue';
+import Video from './twig/components/video/Video.vue';
+import IndexSection from './twig/components/index-section/IndexSection.vue';
+import Table from './twig/components/table/Table.vue';
+import ContextArticleGrid from './twig/components/context-article-grid/ContextArticleGrid.vue';
+import Search from './twig/components/search/Search.vue';
+import Modal from './twig/components/modal/Modal.vue';
+import Accordion from './twig/components/accordion/Accordion.vue';
+import ArticleGrid from './twig/components/article-grid/ArticleGrid.vue';
+import MediaAside from './twig/components/media-aside/MediaAside.vue';
+import EndpointFilter from './twig/components/endpoint-filter/EndpointFilter.vue';
+
+// Animated
+import TextGroup from './twig/components/text-group/TextGroup.vue';
+import ButtonGroup from './twig/components/button-group/ButtonGroup.vue';
+import SectionNav from './twig/components/section-nav/SectionNav.vue';
+import DarkInterstitialFact from './twig/components/dark-interstitial/DarkInterstitialFact.vue';
+import SubpageNav from './twig/components/subpage-nav/SubpageNav.vue';
+import AnimatedBorder from './twig/components/featured-post/AnimatedBorder.vue';
+
+export default {
+    Header,
+    OverlayHero,
+    Carousel,
+    Video,
+    IndexSection,
+    Table,
+    ContextArticleGrid,
+    Search,
+    Modal,
+    VueFinalModal,
+    ModalsContainer,
+    Accordion,
+    TextGroup,
+    ButtonGroup,
+    SectionNav,
+    DarkInterstitialFact,
+    SubpageNav,
+    AnimatedBorder,
+    ArticleGrid,
+    MediaAside,
+    EndpointFilter,
+};
