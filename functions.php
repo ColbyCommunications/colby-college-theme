3900eb83b41dcba6067ca553aa6199a5319df2fa
Giovanni Aceto <55949483+GioAceto@users.noreply.github.com>
2023-11-28 10:35:00 -0500

    remove algolia test function

--- functions.php
+++ functions.php
@@ -2113,15 +2113,3 @@ function on_save_post( $post_id ) {
 	}
 }
 add_action( 'save_post', 'on_save_post' );
-
-function disable_algolia_indexing() {
-    $site_url = $_SERVER['HTTP_HOST'];
-
-    // Check if the site URL is local
-    if (strpos($site_url, 'colby.edu') === false) {
-        // Disable Algolia indexing
-				return false;
-    }
-}
-
-add_filter('algolia_should_index_searchable_post', 'disable_algolia_indexing');
