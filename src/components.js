62cb14e5a6e8f9b484fc4a27ae412754e2d2d7a3
Brandon K. Waltz <bwaltz6@gmail.com>
2024-01-23 16:37:27 -0500

    stuff

--- src/components.js
+++ src/components.js
@@ -10,6 +10,7 @@ import Modal from './twig/components/modal/Modal.vue';
 import Accordion from './twig/components/accordion/Accordion.vue';
 import ArticleGrid from './twig/components/article-grid/ArticleGrid.vue';
 import MediaAside from './twig/components/media-aside/MediaAside.vue';
+import EndpointFilter from './twig/components/endpoint-filter/Endpoint-Filter.vue';
 
 // Animated
 import TextGroup from './twig/components/text-group/TextGroup.vue';
@@ -38,4 +39,5 @@ export default {
     DarkInterstitialFact,
     SubpageNav,
     AnimatedBorder,
+    EndpointFilter,
 };
