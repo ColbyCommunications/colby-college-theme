f76c6933fa316bd8a4b01808c6efa8bfc572da56
Giovanni Aceto <55949483+GioAceto@users.noreply.github.com>
2024-05-29 11:53:21 -0400

    test

--- functions.php
+++ functions.php
@@ -2113,3 +2113,12 @@ function on_save_post( $post_id ) {
 	}
 }
 add_action( 'save_post', 'on_save_post' );
+
+
+function custom_media_library_script() {
+    // Enqueue the script only on the media library page
+    if (is_admin() && isset($_GET['page']) && $_GET['page'] === 'upload.php') {
+        wp_enqueue_script('custom-media-library-script', '/js/custom-media-library-script.js');
+    }
+}
+add_action('admin_enqueue_scripts', 'custom_media_library_script');
