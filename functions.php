f23e39b9a35467354d3d2ff5c5fdb510814278f6
Brandon K. Waltz <bwaltz6@gmail.com>
2025-11-06 08:20:16 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -2415,7 +2415,7 @@ add_action('template_redirect', 'return_404_for_category_archives');
 
 function exclude_specific_posts_from_algolia_index( $should_index, $post ) {
     // Array of post IDs to exclude
-    $excluded_post_ids = array(7443, 7441, 7443, 7439); // Replace these IDs with the IDs of the posts you want to exclude
+    $excluded_post_ids = array(7443, 7441); // Replace these IDs with the IDs of the posts you want to exclude
 
     if ( in_array( $post->ID, $excluded_post_ids ) ) {
         return false;
@@ -2765,7 +2765,7 @@ add_filter('map_meta_cap', function ($caps, $cap, $user_id, $args) {
 
     // === CONFIGURE HERE ===
     // Page IDs to protect
-    $protected_page_ids = [7436, 7441]; // <-- replace with your page IDs
+    $protected_page_ids = [7436, 7441, 7443, 7439]; // <-- replace with your page IDs
 
     // Roles to block from editing those pages
     $blocked_roles = ['editor']; // e.g., block Editors and below
