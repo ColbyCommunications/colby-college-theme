5f67719c48b6f2366c242a67bc48f5888659558d
Brandon K. Waltz <bwaltz6@gmail.com>
2026-03-12 09:36:48 -0400

    Update TextGroup.vue

--- src/twig/components/text-group/TextGroup.vue
+++ src/twig/components/text-group/TextGroup.vue
@@ -14,6 +14,7 @@
                 subheading: undefined,
                 heading: undefined,
                 waypoint: undefined,
+                isBot: false, // Local state for bot detection
             };
         },
         mounted() {
